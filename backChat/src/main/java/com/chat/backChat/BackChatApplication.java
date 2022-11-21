16e12c05aef6925dbb702b5cacfbb560b02ba7c5
Gearted <63016907+Gearted@users.noreply.github.com>
2022-11-21 15:02:06 +0100

    Initial commit

---
+++ backChat/src/main/java/com/chat/backChat/BackChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.backChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackChatApplication.class, args);
+	}
+
+}
---
+++ backChat/src/test/java/com/chat/backChat/BackChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.backChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
